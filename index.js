44694ab8bb704cbd4752df4bb807061f006695fa
avvncor <amaan.imtiyaz@gmail.com>
2019-11-22 02:28:20 +0530

    Improving 0.2

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ app.use('/', require('./routes/index'));
 app.use('/api/url/', require('./routes/url'));
 
 
-// const PORT = process.env.PORT || 5000;
-const PORT =  5000;
+const PORT = process.env.PORT || 5000;
+// const PORT =  5000;
 app.listen(PORT, ()=>{
     console.log(`Server running on PORT ${PORT}`)
 });
